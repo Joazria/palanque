3b352aae027132ee308007dd6647ff1d98467fd8
JoA Azria <joa.azria@gmail.com>
2020-11-14 17:07:20 -0300

    create badge Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_14_200221) do
+ActiveRecord::Schema.define(version: 2020_11_14_200539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "badges", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "candidates", force: :cascade do |t|
     t.string "sq_candidate"
