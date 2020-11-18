7e2bf0292e26b13ae825a7e3d00436dcea5afa8f
JoA Azria <joa.azria@gmail.com>
2020-11-17 21:01:30 -0300

    seed 1 candidate

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+candidate = Candidate.create(
+  sq_candidate: '250000766422',
+  nr_candidate: '65',
+  name_candidate: 'ORLANDO SILVA DE JESUS JUNIOR',
+  party_candidate: 'PC do B',
+  nr_party: '65',
+  total_patrimony: '738360.3',
+  ds_coligation_composition: 'PC do B',
+  ds_nacionality: 'BRASILEIRA NATA',
+  birth_dt: '27/05/1971',
+  ds_gender: 'MASCULINO',
+  ds_marital_status: 'CASADO(A)',
+  ds_color_race: 'PRETA',
+  ds_ocupation: 'DEPUTADO',
+  militancy: '#',
+  acting_region: 'SÃO PAULO',
+  candidate_ballot_name: 'ORLANDO SILVA',
+  email_candidate: 'PCDOB2020@GMAIL.COM',
+  running_to: 'PREFEITO'
+)
