e7b76fd587a7241c9f08201a2c2483eff2907c97
camilo micheletto <camilomichelettors@gmail.com>
2020-12-05 17:03:40 -0300

    --no-edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,10 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-protected
+  protected
   def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar, :username])
      devise_parameter_sanitizer.permit(:account_update, keys: [:avatar, :username])
-
   end
 end
 
